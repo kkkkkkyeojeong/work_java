17fbdd9120fb566545874af411f4297323f1991e
KOITT <KOITT@KOITT-PC17>
2018-01-03 10:04:32 +0900

    자바 5장 Car 수정 후

--- ch05/src/obj/constructor/Car.java
+++ ch05/src/obj/constructor/Car.java
@@ -3,8 +3,8 @@ package obj.constructor;
 public class Car {
 	
 	private int maxSpeed;
-	public String brandName;
-	public int speed;
+	private String brandName;
+	private int speed;
 	
 	public Car(String brandName) {
 		this.brandName = brandName;
@@ -15,12 +15,34 @@ public class Car {
 		this.maxSpeed = maxSpeed;
 	}
 	
+	// 속도관련 메소드
 	public int speedUp() {
-		return this.speed+=30;
+		int tempspeed = this.speed;
+		tempspeed += 30;
+		
+		// 최대 속도보다 더 높다면 이전 속도 유지
+		if (tempspeed > this.maxSpeed) {
+			return this.speed;
+		}
+		// 최대 속도보다 같거다 작다면 현재 속도를 갱신
+		this.speed = tempspeed;
+		
+		return this.speed;
 	}
 	
 	public int speedDown() {
-		return this.speed -=20;
+		// 현재 속도를 임시 저장
+		int Dspeed = this.speed;
+		Dspeed -= 20;
+		// 현재 속도가 0보다 작다면
+		if (Dspeed < 0) {
+			//return this.speed;
+			return 0;
+		}
+		// 현재 속도를 계산한 속도로 갱신 
+		this.speed = Dspeed;
+		
+		return this.speed;
 	}
 	
 	public int getMaxSpeed() {
