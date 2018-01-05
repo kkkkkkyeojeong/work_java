73bc5f49a140a1f3d1f3fa9906c24e833bdb80d2
VIP <VIP@VIP-PC>
2018-01-05 22:07:30 +0900

    자바 6장 프로그래밍연습 5번 수정

--- ch06/src/practice/Car.java
+++ ch06/src/practice/Car.java
@@ -5,63 +5,90 @@ public class Car {
 	int maxSpeed;	// 최대속도
 	int speed;		// 현재속도
 	
+	// 생성자 
 	public Car(int maxSpeed) {
 		super();
 		this.maxSpeed = maxSpeed;
 	}
 	
 	public int speedUp() {
-		speed += 5;
-		this.speed = speed;
-		if (this.speed > this.maxSpeed) {
+		int a = this.speed;
+		a += 5;
+		
+		// 최대속도보다 높을 경우
+		if (a > maxSpeed) {
+			System.out.println("최대속도: " + this.maxSpeed + ", 현재속도: " + this.speed);
 			this.speed = this.maxSpeed;
-			return maxSpeed;
+			return this.speed;
 		}
-		System.out.println("최대속도: " + this.maxSpeed + ", 현재속도: " + this.speed);
-		return this.speed;
+		// 최대속도보다 높지 않을 경우 
+		else {
+			this.speed = a;
+			System.out.println("최대속도: " + this.maxSpeed + ", 현재속도: " + this.speed);
+			return this.speed;
+		}
 	}
 	
 	public int speedUp(int speed) {
 		int a = this.speed;
+		a += speed;
+		
+		// 입력받은 speed가 0보다 작을 경우 
 		if (speed < 0) {
-			speed = 0;
+			System.out.println("최대속도: " + this.maxSpeed + ", 현재속도: " + this.speed);
+			return this.speed;
 		}
-			if(speed >= 300 ) {
-				speed = 0;
+			// speed가 최대속도를 넘을 경우 
+			else if (a > this.maxSpeed) {
 				this.speed = this.maxSpeed;
+				System.out.println("최대속도: " + this.maxSpeed + ", 현재속도: " + this.speed);
 				return this.speed;
 			}
-		a +=speed;
-	
-		this.speed = a;
-		System.out.println("최대속도: " + this.maxSpeed + ", 현재속도: " + this.speed);
-		return this.speed;
+			else {	
+				this.speed = a;
+				System.out.println("최대속도: " + this.maxSpeed + ", 현재속도: " + this.speed);
+				return this.speed;
+			}	
 	}
-	
+		
 	public int speedDown() {
-		speed -= 5;
-		this.speed = speed;
-		if (this.speed < 0) {
+		int a = this.speed;
+		a -= 5;
+		
+		// 속도가 0 이하일 경우 
+		if (a < 0) {
+			System.out.println("최대속도: " + this.maxSpeed + ", 현재속도: " + this.speed);
 			this.speed = 0;
 			return this.speed;
 		}
-		System.out.println("최대속도: " + this.maxSpeed + ", 현재속도: " + this.speed);
-		return this.speed;
+		// 최대속도보다 높지 않을 경우 
+		else {
+			this.speed = a;
+			System.out.println("최대속도: " + this.maxSpeed + ", 현재속도: " + this.speed);
+			return this.speed;
+		}
 	}
 	
 	public int speedDown(int speed) {
 		int a = this.speed;
+		a -= speed;
+		
+		// 입력받은 speed가 0보다 작을 경우 
 		if (speed < 0) {
-			speed = 0;
+			System.out.println("최대속도: " + this.maxSpeed + ", 현재속도: " + this.speed);
+			return this.speed;
 		}
-			if(this.speed < 0 ) {
+			// speed가 0이하 일 경우 
+			else if (a < 0) {
 				this.speed = 0;
+				System.out.println("최대속도: " + this.maxSpeed + ", 현재속도: " + this.speed);
 				return this.speed;
 			}
-		a -=speed;
-		this.speed = a;
-		System.out.println("최대속도: " + this.maxSpeed + ", 현재속도: " + this.speed);
-		return this.speed;
+			else {	
+				this.speed = a;
+				System.out.println("최대속도: " + this.maxSpeed + ", 현재속도: " + this.speed);
+				return this.speed;
+			}	
 	}
 	
 	public static void main(String[] args) {
