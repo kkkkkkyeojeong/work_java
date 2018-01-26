91c079c1b9fd1829bd75cc6fe005e35fbf5e82cd
KOITT <KOITT@KOITT-PC17>
2018-01-26 17:03:02 +0900

    BOARD게시판-count 삭제 완료

--- KoittBoard/src/com/koitt/java/board/service/BoardService.java
+++ KoittBoard/src/com/koitt/java/board/service/BoardService.java
@@ -11,19 +11,13 @@ import com.koitt.java.board.model.Board;
 public class BoardService {
 	
 	private BoardDao dao;
-	private Integer count;	// 기본값이 null 이기 때문에 생성자에서 0으로 초기화해야 한다.
-	//private Board Numcount;
 	
 	public BoardService() {
 		this.dao = new BoardDao();
-		this.count = 0;	// 0으로 초기화
-		//this.Numcount = new Board();
 	}
 	
 								// 3.
 	public void add(Board board) throws BoardException, SQLException {
-		board.setId(++this.count);		// null값이었던 id값을 채워준다.
-		board.setRegDate(new Date());	// new Date() 하는 순간의 시간이 저장된다.
 		this.dao.insert(board);			// 자료구조(ArrayList)에 저장하기 위해 dao로 board 객체를 전달
 	}
 	
